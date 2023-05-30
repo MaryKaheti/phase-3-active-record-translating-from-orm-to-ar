2dbb4ec75eff05b8fac470e5eda5caeace04f96a
MaryKaheti <kahetikihara@gmail.com>
2023-05-30 11:35:20 +0300

    Initial Commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,15 @@
-class Dog
+#require "pry"
+#require "active_record"
+#require_relative "../config/environment"
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+
+)
+
+
+
+
+
+class Dog < ActiveRecord::Base
 end
